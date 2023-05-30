91b0296d7fae00e08fe6bd25a3b3d39d23e1fb7e
Kristi Ballolli <kristianballolli1@gmail.com>
2023-05-30 17:35:37 +0200

    Adding an Web Form Ui in XRetailManager system
    Adding Login and register Page.
    Calling api for authenticating User.

---
+++ XRetailManagerUI/Account/Login.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using XRetailManagerUI.Helpers;
+
+namespace XRetailManagerUI.Account
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        ApiHelper api = ApiHelper.Instance;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected async void LogIn_Click(object sender, EventArgs e)
+        {
+            lblLoginValidation.Text = "";
+            var result = await api.AuthenticateUser(Email.Text, Password.Text);
+
+            if ((result.Access_Token == null && result.Username == null) && !string.IsNullOrEmpty(result.Error))
+            {
+                lblLoginValidation.Text = $"Ann error occured: {result.Error}.";
+            }
+          
+        }
+    }
+}
---
+++ XRetailManagerUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XRetailManagerUI.Global" Language="C#" %>
